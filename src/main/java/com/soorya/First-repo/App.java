bef5e5c0686d1a004d9b91df3e745f7627693a93
soorya <sooryasoosh@gmail.com>
2022-01-08 18:45:26 +0530

    First commit

---
+++ src/main/java/com/soorya/First-repo/App.java
@@ -0,0 +1,14 @@
+package com.soorya.First-repo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        System.out.println("This is Soorya!");
+    }
+}
